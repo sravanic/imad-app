b4d2bea639689304c9930ab009c6dee4eb853934
sravanic <30699569+sravanic@users.noreply.github.com>
2017-08-17 18:25:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+//change the text.
 var element=document.getElementById('main-text');
 element.innerHTML='NEW Value';
+//Change the image postion
+var img=document.getElementById('mad');
+img.onclick=function(){
+    img.style.marginLeft='100px'
+}
