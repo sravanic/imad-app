e587572a5077921a0083bc4664d01f26cece7de9
sravanic <30699569+sravanic@users.noreply.github.com>
2017-08-17 22:46:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,6 +108,7 @@ app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName
   res.send(createTemplate(articles[articleName]));
 });
+
 var counter=0;
 app.get('/counter', function (req, res) {
      counter=counter+1
