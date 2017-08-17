885d0307ec7d578876f7bf1b49ec9f6e364c8fe7
sravanic <30699569+sravanic@users.noreply.github.com>
2017-08-17 19:31:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,13 @@ var element=document.getElementById('main-text');
 element.innerHTML='NEW Value';
 //Change the image postion
 var img=document.getElementById('mad');
+var marginLeft=0;
+function moveright()
+{
+   marginLeft= marginLeft+10;
+    img.style. marginLeft= marginLeftt+'px'
+}
 img.onclick=function(){
-    img.style.marginLeft='100px'
+    var interval=setInterval(moveright,100)
+   // img.style.marginLeft='100px'
 }
