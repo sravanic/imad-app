fbd58b06d3e4b222b0df670da53e7f8d48a4cb66
sravanic <30699569+sravanic@users.noreply.github.com>
2017-08-19 00:26:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,6 +109,21 @@ return htmlTemplate;
 }
 
 
+var pool=new Pool(config);
+
+app.get('/test-db', function (req, res)
+{
+  pool.query('SELECT * FROM SRAVANI_TEST',function(err,result)
+  {
+      if(err){
+          res.status(500).send(err.toString());
+      }
+      else{
+          res.send(JSON.stringify(result));
+      }
+      
+  });  
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
